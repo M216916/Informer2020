9f586cd93f65760be2f6ace6cf77d074c5a79997
M216916 <86331863+M216916@users.noreply.github.com>
2022-08-05 10:55:36 +0900

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -235,7 +235,7 @@ class Dataset_Custom(Dataset):
             cols.remove(self.target)
         else:
             cols = list(df_raw.columns); cols.remove(self.target); cols.remove('date')
-        df_raw = df_raw[['date']+cols+[self.target]]　　　　　　　　　　　　　　　　　　#df_raw:(5159,9)
+        df_raw = df_raw[['date']+cols+[self.target]]                                   #df_raw:(5159,9)
 
         num_train = int(len(df_raw)*0.7)                                               #3611(5159*0.7)
         num_test = int(len(df_raw)*0.2)                                                #1031(5159*0.2)
@@ -261,9 +261,9 @@ class Dataset_Custom(Dataset):
             
             
 #//////////////////////////////////////////////////////////////////////////////////////////////////////           
-        print('\n▼cals_data')
-        print(cals_data)
-        print(cals_data.shape)
+        print('\n▼cols_data')
+        print(cols_data)
+        print(cols_data.shape)
         
         print('\n▼df_data')
         print(df_data)
