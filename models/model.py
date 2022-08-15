826b05d328da2f2cc5f4fcfc4d03a566eda4a602
M216916 <86331863+M216916@users.noreply.github.com>
2022-08-15 14:43:16 +0900

    Update model.py

--- models/model.py
+++ models/model.py
@@ -30,24 +30,6 @@ class Informer(nn.Module):
         # Encoding
         self.enc_embedding = DataEmbedding(enc_in, d_model, embed, freq, dropout)
         self.dec_embedding = DataEmbedding(dec_in, d_model, embed, freq, dropout)
-        
-#//////////////////////////////////////////////////////////////////////////////////////////////////////
-# ▼self.enc_embedding
-# DataEmbedding(
-#   (value_embedding)   : TokenEmbedding((tokenConv): Conv1d(8, 512, kernel_size=(3,), stride=(1,), padding=(1,), padding_mode=circular))
-#   (position_embedding): PositionalEmbedding()
-#   (temporal_embedding): TimeFeatureEmbedding((embed): Linear(in_features=5, out_features=512, bias=True))
-#   (dropout): Dropout(p=0.05, inplace=False)
-# )
-
-# ▼self.dec_embedding
-# DataEmbedding(
-#   (value_embedding): TokenEmbedding((tokenConv): Conv1d(8, 512, kernel_size=(3,), stride=(1,), padding=(1,), padding_mode=circular))
-#   (position_embedding): PositionalEmbedding()
-#   (temporal_embedding): TimeFeatureEmbedding((embed): Linear(in_features=5, out_features=512, bias=True))
-#   (dropout): Dropout(p=0.05, inplace=False)
-# )
-#//////////////////////////////////////////////////////////////////////////////////////////////////////
 
         # Attention
         Attn = ProbAttention if attn=='prob' else FullAttention
@@ -106,6 +88,93 @@ class Informer(nn.Module):
             return dec_out[:,-self.pred_len:,:], attns
         else:
             return dec_out[:,-self.pred_len:,:] # [B, L, D]
+        
+#//////////////////////////////////////////////////////////////////////////////////////////////////////
+# ▼self.enc_embedding
+# DataEmbedding
+#   ┣━(value_embedding)   : TokenEmbedding((tokenConv): Conv1d(8, 512, kernel_size=(3,), stride=(1,), padding=(1,), padding_mode=circular))
+#   ┣━(position_embedding): PositionalEmbedding()
+#   ┣━(temporal_embedding): TimeFeatureEmbedding((embed): Linear(in_features=5, out_features=512, bias=True))
+#   ┗━(dropout)           : Dropout(p=0.05, inplace=False)
+
+# ▼self.dec_embedding
+# DataEmbedding
+#   ┣━(value_embedding)   : TokenEmbedding((tokenConv): Conv1d(8, 512, kernel_size=(3,), stride=(1,), padding=(1,), padding_mode=circular))
+#   ┣━(position_embedding): PositionalEmbedding()
+#   ┣━(temporal_embedding): TimeFeatureEmbedding((embed): Linear(in_features=5, out_features=512, bias=True))
+#   ┗━(dropout)           : Dropout(p=0.05, inplace=False)
+
+# ▼self.encoder
+# Encoder
+#  (attn_layers): ModuleList
+#   ┣━(0): EncoderLayer
+#   ┃  ┣━(attention): AttentionLayer
+#   ┃  ┃  ┣━(inner_attention): ProbAttention((dropout): Dropout(p=0.05, inplace=False))
+#   ┃  ┃  ┣━(query_projection): Linear(in_features=512, out_features=512, bias=True)
+#   ┃  ┃  ┣━(key_projection): Linear(in_features=512, out_features=512, bias=True)
+#   ┃  ┃  ┣━(value_projection): Linear(in_features=512, out_features=512, bias=True)
+#   ┃  ┃  ┗━(out_projection): Linear(in_features=512, out_features=512, bias=True)
+#   ┃  ┃    
+#   ┃  ┣━(conv1): Conv1d(512, 2048, kernel_size=(1,), stride=(1,))
+#   ┃  ┣━(conv2): Conv1d(2048, 512, kernel_size=(1,), stride=(1,))
+#   ┃  ┣━(norm1): LayerNorm((512,), eps=1e-05, elementwise_affine=True)
+#   ┃  ┣━(norm2): LayerNorm((512,), eps=1e-05, elementwise_affine=True)
+#   ┃  ┗━(dropout): Dropout(p=0.05, inplace=False)
+#   ┃ 
+#   ┗━(1): EncoderLayer
+#      ┣━(attention): AttentionLayer
+#      ┃  ┣━(inner_attention): ProbAttention((dropout): Dropout(p=0.05, inplace=False))
+#      ┃  ┣━(query_projection): Linear(in_features=512, out_features=512, bias=True)
+#      ┃  ┣━(key_projection): Linear(in_features=512, out_features=512, bias=True)
+#      ┃  ┣━(value_projection): Linear(in_features=512, out_features=512, bias=True)
+#      ┃  ┗━(out_projection): Linear(in_features=512, out_features=512, bias=True)
+#      ┃ 
+#      ┣━(conv1): Conv1d(512, 2048, kernel_size=(1,), stride=(1,))
+#      ┣━(conv2): Conv1d(2048, 512, kernel_size=(1,), stride=(1,))
+#      ┣━(norm1): LayerNorm((512,), eps=1e-05, elementwise_affine=True)
+#      ┣━(norm2): LayerNorm((512,), eps=1e-05, elementwise_affine=True)
+#      ┗━(dropout): Dropout(p=0.05, inplace=False)
+#      
+#  (conv_layers): ModuleList
+#   ┗━(0): ConvLayer
+#      ┣━(downConv): Conv1d(512, 512, kernel_size=(3,), stride=(1,), padding=(1,), padding_mode=circular)
+#      ┣━(norm): BatchNorm1d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+#      ┣━(activation): ELU(alpha=1.0)
+#      ┗━(maxPool): MaxPool1d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
+#
+#  (norm): LayerNorm((512,), eps=1e-05, elementwise_affine=True)
+
+
+# ▼self.decoder
+# Decoder
+#  (layers): ModuleList(
+#   ┗━(0): DecoderLayer
+#      ┣━(self_attention): AttentionLayer
+#      ┃  ┣━(inner_attention): ProbAttention((dropout): Dropout(p=0.05, inplace=False))
+#      ┃  ┣━(query_projection): Linear(in_features=512, out_features=512, bias=True)
+#      ┃  ┣━(key_projection): Linear(in_features=512, out_features=512, bias=True)
+#      ┃  ┣━(value_projection): Linear(in_features=512, out_features=512, bias=True)
+#      ┃  ┗━(out_projection): Linear(in_features=512, out_features=512, bias=True)
+#      ┃
+#      ┣━(cross_attention): AttentionLayer(
+#      ┃  ┣━(inner_attention): FullAttention((dropout): Dropout(p=0.05, inplace=False))
+#      ┃  ┣━(query_projection): Linear(in_features=512, out_features=512, bias=True)
+#      ┃  ┣━(key_projection): Linear(in_features=512, out_features=512, bias=True)
+#      ┃  ┣━(value_projection): Linear(in_features=512, out_features=512, bias=True)
+#      ┃  ┗━(out_projection): Linear(in_features=512, out_features=512, bias=True)
+#      ┃
+#      ┣━(conv1): Conv1d(512, 2048, kernel_size=(1,), stride=(1,))
+#      ┣━(conv2): Conv1d(2048, 512, kernel_size=(1,), stride=(1,))
+#      ┣━(norm1): LayerNorm((512,), eps=1e-05, elementwise_affine=True)
+#      ┣━(norm2): LayerNorm((512,), eps=1e-05, elementwise_affine=True)
+#      ┣━(norm3): LayerNorm((512,), eps=1e-05, elementwise_affine=True)
+#      ┗━(dropout): Dropout(p=0.05, inplace=False)
+#
+#  (norm): LayerNorm((512,), eps=1e-05, elementwise_affine=True)
+
+# ▼self.projection
+#   Linear(in_features=512, out_features=8, bias=True)
+#//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
         
