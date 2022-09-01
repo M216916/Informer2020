d450a80520e2fa0fd8d3ab11f5455dd17d57aa06
M216916 <86331863+M216916@users.noreply.github.com>
2022-09-01 14:52:14 +0900

    Update embed.py

--- models/embed.py
+++ models/embed.py
@@ -26,15 +26,19 @@ class PositionalEmbedding(nn.Module):
 class TokenEmbedding(nn.Module):
     def __init__(self, c_in, d_model):
         super(TokenEmbedding, self).__init__()
-        padding = 1 if torch.__version__>='1.5.0' else 2
-        self.tokenConv = nn.Conv1d(in_channels=c_in, out_channels=d_model, 
-                                    kernel_size=3, padding=padding, padding_mode='circular')
+        padding = 1 if torch.__version__>='1.5.0' else 2                                         # padding = 1
+        self.tokenConv = nn.Conv1d(in_channels=c_in, out_channels=d_model,                       # c_in = 8
+                                    kernel_size=3, padding=padding, padding_mode='circular')     # d_model = 512
+        
         for m in self.modules():
             if isinstance(m, nn.Conv1d):
                 nn.init.kaiming_normal_(m.weight,mode='fan_in',nonlinearity='leaky_relu')
 
     def forward(self, x):
-        x = self.tokenConv(x.permute(0, 2, 1)).transpose(1,2)
+        x = self.tokenConv(x.permute(0, 2, 1)).transpose(1,2)   　　　　　　　　　　　　　　　　　# x:(32,96,8)
+                                                                                                 # → permute:(32,8,96)
+                                                                                                 # → tokenConv:(32,512,96)
+                                                                                                 # → transpose:(32,512,96)
         return x
 
 class FixedEmbedding(nn.Module):
